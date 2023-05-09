60136b0ca9f4462bbed457b065b7f8ef5ff0e0fa
Dmitriuwrfhi <dimasmix27@gmail.com>
2023-05-09 15:05:31 +0300

    Вирішив щось там типу два елементи та масив мабудь.

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static int[] twoSum(int[] nums, int target){
         int[] answer = new int [2];
         for (int i = 0; i < nums.length; i++){
-            for (int j = i+1; j < nums.length; j++){
-                if (nums[i] + nums[j] == target) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target){
                     answer[0] = i;
                     answer[1] = j;
                 }
