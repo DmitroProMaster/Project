e79dedc1ed909f3ac8e28015fb86aab9265c11a6
Dmitriuwrfhi <dimasmix27@gmail.com>
2023-05-09 14:55:20 +0300

    Вирішив щось там типу два елементи та масив мабудь.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        for (int i =1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        int[] nums = {2, 7, 11, 15};
+        int target = 17;
+        System.out.println(twoSum(nums, target));
+    }
+    public static int[] twoSum(int[] nums, int target){
+        int[] answer = new int [2];
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i+1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target) {
+                    answer[0] = i;
+                    answer[1] = j;
+                }
+            }
         }
+        return answer;
     }
 }
